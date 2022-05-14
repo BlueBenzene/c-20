a4a148ff2a4989981d5118ea0388dfa4194e1adc
blue <swastiksrichandan3@gmail.com>
2022-05-14 18:34:06 +0530

    string

---
+++ listing0305.cpp
@@ -0,0 +1,20 @@
+// print average , testing for a zero count 
+// print nothing if no input
+
+#include <iostream>
+
+int main () {
+    int sum{0};
+    int count{};
+    int x;
+    while (std::cin >> x)
+    {
+        sum += x;
+        ++count;
+    }
+    if (count = 0 )
+    std::cout << "No Data.\n";
+    else
+        std::cout << "average = " << sum / count << '\n';
+    
+}
