c24394270df4983860c01a1ce05fcfa83c944693
blue <swastiksrichandan3@gmail.com>
2022-05-18 21:05:57 +0530

    String complete

---
+++ listing0401.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main()
+{
+    std::cout << "\nCircle\t\t?\n"
+                 "Triangle\t"
+              << 3 << '\n';
+    std::cout << "\nCircle\t\t?\nTriangle\t" << 3 << '\n';
+    std::cout << "\n"
+                 "Circle"
+                 "\t\t?\n"
+                 "Triangle"
+                 "\t"
+              << 3 << '\n';
+}
---
+++ listing0405.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+
+int main () {
+    std:: string empty;
+    std::cout << "|" << empty << "|\n";
+}
